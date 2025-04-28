bd3b7316327caf8277d4ffe897eebee9a2699f7f
Juan Bonaventura <juan.bonaventura04@gmail.com>
2025-04-27 23:58:23 -0300

    javascript & events - CSS Styles & Index Changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,24 +2,28 @@ const products = [
     {
         name: "Nintendo Gamecube - Consola Nintendo",
         description: "usado",
-        image: "./imgs/gamecube.jpg",
+        image: "./imgs/gamecube.png",
         price: "300.000"
     },
     {
         name: "Playstation 1 - Consola Sony",
         description: "usado",
-        image: "./imgs/playstation-1.jpg",
+        image: "./imgs/playstation-1.png",
         price: "160.000"  
     },
     {
         name: "Playstation 3 Slim - Consola Sony",
         description: "nuevo",
-        image: "./imgs/playstation-3.jpg",
+        image: "./imgs/playstation-3.png",
         price: "400.000"
     }
 ];
 
 const grid = document.querySelector('.product-space');
+const searchInput = document.querySelector('#input-search-products');
+const stateNewCheckBox = document.querySelector('#state-new');
+const stateUsedCheckBox = document.querySelector('#state-used');
+const stateReCheckBox = document.querySelector('#state-re');
 
 function createProductCard(product){
     const card = document.createElement('article');
@@ -62,7 +66,37 @@ function addProduct() {
     grid.appendChild(card);
 }
 
-products.forEach( product => {
-    const card = createProductCard(product);
-    grid.appendChild(card);
+function renderProducts(list){
+    list.forEach( product => {
+        const card = createProductCard(product);
+        grid.appendChild(card);
+    });
+}
+
+function filterProducts(text){
+    const filteredProducts = products.filter( product => {
+    const newState = !stateNewCheckBox.checked || product.description == "nuevo";
+    const usedState = !stateUsedCheckBox.checked || product.description == "usado";
+    const reState = !stateReCheckBox.checked || product.description == "reacondicionado";
+
+        return product.name.toLowerCase().includes(text.toLowerCase()) && newState && usedState && reState;
+    });
+    grid.innerHTML = '';
+    renderProducts(filteredProducts);
+}
+
+searchInput.addEventListener('input', (e) => {
+    filterProducts(e.target.value);
 });
+
+stateNewCheckBox.addEventListener('change', (e) => {
+    filterProducts(searchInput.value);
+})
+stateUsedCheckBox.addEventListener('change', (e) => {
+    filterProducts(searchInput.value);
+})
+stateReCheckBox.addEventListener('change', (e) => {
+    filterProducts(searchInput.value);
+})
+
+renderProducts(products);
